8172d1b918a8e331b0ece81f5dff2cdf482493c3
Ankita Nandkumar Patil <ankitanp31@gmail.com>
2025-09-14 15:25:03 +0530

    auth apis

---
+++ src/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser")
+const bcrypt = require("bcrypt")
+const connectDB = require("./config/database");
+const app = express();
+
+app.use(express.json())  //middleware to fetch dynamic data
+app.use(cookieParser());
+
+// importing the apis from routes
+const authRouter = require("./apiRoutes/auth")
+const profileRouter = require("./apiRoutes/profile");
+
+
+// let the app use our apis
+app.use("/", authRouter)
+app.use("/", profileRouter)
+
+
+connectDB()
+  .then(() => {
+    console.log("DB connection established successfully");
+    app.listen(3000, () => {
+      console.log("app listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("error connecting to DB : ", err);
+  });
